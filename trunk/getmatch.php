0e21352736cb397dd026d39d7eab522fc0e3db5c
HuuNam0@gmail.com <HuuNam0@gmail.com@2997a55e-4993-02b6-7c50-1d17f1e8b227>
2012-08-18 17:37:43 +0000

    test getmatch.php

--- trunk/getmatch.php
+++ trunk/getmatch.php
@@ -8,6 +8,27 @@
 		$result=mysql_query($sql);
 		$match=array();
 		if ($row=mysql_fetch_array($result)) {
+			$match['st']=$row['status'];
+			$match['mi']=$row['minutes'];
+			$match['hg']=$row['hgoals'];
+			$match['ag']=$row['agoals'];
+			$match['h1']=$row['h1goals'];
+			$match['a1']=$row['a1goals'];
+			$match['hr']=$row['hreds'];
+			$match['ar']=$row['areds'];
+			$match['hy']=$row['hyellows'];
+			$match['ay']=$row['ayellows'];
+			$match['hs']=$row['hshots'];
+			$match['as']=$row['ashots'];
+			$match['hsg']=$row['hgshots'];
+			$match['asg']=$row['agshots'];
+			$match['hc']=$row['hcorner'];
+			$match['ac']=$row['acorner'];
+			$match['hp']=$row['hpossession'];
+			$match['ap']=$row['apossession'];
+			$match['he']=$row['hpenalty'];
+			$match['ae']=$row['apenalty'];
+			/*
 			$match['st']=($row['status']?$row['status']:"");
 			$match['mi']=($row['minutes']?$row['minutes']:"");
 			$match['hg']=($row['hgoals']?$row['hgoals']:"");
@@ -28,6 +49,7 @@
 			$match['ap']=($row['apossession']?$row['apossession']:"");
 			$match['he']=($row['hpenalty']?$row['hpenalty']:"");
 			$match['ae']=($row['apenalty']?$row['apenalty']:"");
+			*/
 		} 
 		echo json_encode($match);
 	}
