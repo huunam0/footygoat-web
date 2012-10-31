aaf4a59d4cef1d0a01b307cfda8f6f3def760f59
HuuNam0@gmail.com <HuuNam0@gmail.com@2997a55e-4993-02b6-7c50-1d17f1e8b227>
2012-10-31 04:12:52 +0000

    Update database and trigger.php

--- trunk/trigger.php
+++ trunk/trigger.php
@@ -1,16 +1,51 @@
 <?php
+include_once("maincore.php");
+include_once("dbconfig.php");
+if (isset($_GET['edit'])) {
 if (isset($_POST['submit'])) {
-	print_r($_POST['trigger']);
-	echo "<br/>";
-	print_r($_POST['operator']);
-	echo "<br/>";
-	print_r($_POST['number']);
+	$nb = count($_POST['trigger']);
+	$sql="";
+	for ($i=0;$i<$nb; $i++) {
+		if ($_POST['number'][$i]) {
+			if ($sql) $sql.=",";
+			$sql.="($myid,".$_POST['team'][$i].",".$_POST['trigger'][$i].",'".$_POST['operator'][$i]."',".$_POST['number'][$i].")";
+		}
+	}
+	if ($sql) {
+		$sql="insert into f_condition (`user_id`,`isaway`,`field_id`,`operater`,`value`) Value ".$sql;
+	}
+	$sql2="delete from f_condition where user_id=".$myid;
+	$ret=mysql_query($sql2) or die(mysql_error());
+	$ret=mysql_query($sql) or die(mysql_error());
+	echo "<h2>Your triggers are saved:</h2>";
+	$sql="select * from f_condition,f_fields where f_condition.field_id=f_fields.field_id and user_id=$myid";
+	/**/
+	$ret=mysql_query($sql) or die(mysql_error());
+	$triggersm="";
+	$triggerst="";
+	while ($row = mysql_fetch_array($ret)) {
+		//echo "<div class='oldtrigger'><span style='display:none;'>".$row['trigger_id']."</span>".($row['isaway']?"Away":"Home")." - ".$row['field_name']." ".$row['operater']." ".$row['value']."</div>";
+		if ($row['field_in']=='m') {
+			if ($triggersm) $triggersm.=" and ";
+			$triggersm.="(".($row['isaway']?"a":"h").$row['field_tag'].$row['operater'].$row['value'].")";
+		} else {
+			if ($triggerst) $triggerst.=" and ";
+			$triggerst.="(team_".($row['isaway']?"a":"h").$row['field_tag'].$row['operater'].$row['value'].")";
+		}
+	}
+	mysql_query("delete from f_trigger where user_id=$myid") or die (mysql_error());
+	$sql="insert into f_trigger (user_id,triggersm,triggerst) VALUE ($myid,'".$triggersm."','".$triggerst."');";
+	mysql_query($sql) or die (mysql_error());
+	header("location:trigger.php");
+	//echo $triggersm;
+	
+	/**/
 }
 else {
-	include_once("maincore.php");
+	
 	include_once("header.php");
-	include_once("dbconfig.php");
-	$sql="select * from f_fields";
+	
+	$sql="select * from f_fields order by field_order";
 	$ret = mysql_query($sql);
 	$conds="";
 	if ($ret)
@@ -21,6 +56,8 @@ else {
 ?>
 <script type="text/javascript">
 $(document).ready(function(){
+	//$("#condboard").append('<div class="onetrigger" style="overflow: auto;"> <select name="team[]" class="team"><option value="0">Home</option><option value="1">Away</option></select> <select name="trigger[]" class="chon">'+toption+'</select> <select name="operator[]" class="step2"><option>&gt;</option><option>&ge;</option><option>=</option><option>&lt;</option><option>&le;</option><option>&ne;</option></select> <input name="number[]" type="text" size="2" maxlength="3"  class="step3"/> .</div>');
+	
 	$("#addcond").click(function(){
 		//if ($("#condboard").fi)
 		var isfilled = true;
@@ -31,63 +68,59 @@ $(document).ready(function(){
 			}
 		});
 		if (isfilled)
-		$("#condboard").append('<div class="onetrigger" style="overflow: auto;"> <select name="team[]" class="team"><option value="0">Home</option><option value="1">Away</option></select><select name="trigger[]" class="chon">'+toption+'</select> <select name="operator[]" class="step2"><option>=</option><option>&gt;</option><option>&lt;</option><option>&le;</option><option>&ge;</option><option>&ne;</option></select> <input name="number[]" type="text" size="2" maxlength="3"  class="step3"/> .</div>');
+		$("#condboard").append('<div class="onetrigger" style="overflow: auto;"> <select name="team[]" class="team"><option value="0">Home</option><option value="1">Away</option></select> <select name="trigger[]" class="chon">'+toption+'</select> <select name="operator[]" class="step2"><option>&gt;</option><option>&gt;=</option><option>=</option><option>&lt;</option><option>&lt;=</option><option>&lt;&gt;</option></select> <input name="number[]" type="text" size="2" maxlength="3"  class="step3"/> .</div>');
 		//alert(toption);==?
 	});
-	/*
-	$(".onetrigger").hover(
-	function(){
-		$(this).append('<span class="listtodo">Delete</span>');
-		//alert("OK");
-	},
-	function (){
-		$(this).find("span:last").remove();
-		//$("#addcond").html("Out");
-	}
-	);
-	*/
-	/*
-	$("span").click(function(){
-		alert($(this).text());
-	});
-	$(".onetrigger:last").live({
-		mouseover:function(){
-			$(this).append('<span>Add trigger</span>');
-		},
-		mouseout:function(){
-			$(this).find("span:last").remove();
-		}
-	});
-	$(".onetrigger").live({
-		mouseover:function(){
-			$(this).append('<span>Delete</span>');
-		},
-		mouseout:function(){
-			$(this).find("span:last").remove();
-		}
-	});
-	*/
 	$(".todo").live("click",function(){
-		alert($(this).text());
+		//if (confirm("Do you really want to delete this condition?\n"+$(this).parent().text())==true)
+		$(this).parent().remove();
+	});
+	$(".todid").live("click",function(){
+		//if (confirm("Do you really want to delete this condition?\n"+$(this).parent().text())==true)
+		alert($(this).parent().find("span:first").text());
 	});
 	$(".onetrigger").live({
 		mouseenter:function(){
+			$(this).css("background-color","#afff00");
 			$(this).append('<span class="todo">Delete this trigger</span> ');
 		},
 		mouseleave:function(){
+			$(this).css("background-color","#ffffff");
 			$(this).find("span.todo").remove();
 		}
 	});
-	
+	$(".oldtrigger").hover(
+		function(){
+			$(this).css("background-color","#afff00");
+			$(this).append('<span class="todid">Delete this trigger</span> ');
+		},
+		function(){
+			$(this).css("background-color","#ffffff");
+			$(this).find("span.todid").remove();
+		}
+	);
+	//$("#addcond").click();
 });
 </script>
 </head>
 <body>
 <h2>Set your own triggers </h2>
-<form name="input" action="?" method="post">
+<form name="input" action="?edit" method="post">
 <div id="condboard">
+<?php
+$sql="select * from f_condition,f_fields where f_condition.field_id=f_fields.field_id and user_id=$myid";
+$ret=mysql_query($sql);
+$i=1;
+while ($row = mysql_fetch_array($ret)) {
+	//echo "<div class='oldtrigger'><span style='display:none;'>".$row['trigger_id']."</span>".($row['isaway']?"Away":"Home")." - ".$row['field_name']." ".$row['operater']." ".$row['value']."</div>";
+	echo '<div class="onetrigger" style="overflow: auto;"> <select name="team[]" class="team"><option value="0">Home</option><option value="1" '.($row['isaway']?"selected":"").'>Away</option></select> <select id="trigger'.$i.'" name="trigger[]" class="chon">'.$conds.'</select> <select id="operator'.$i.'" name="operator[]" class="step2"><option>&gt;</option><option>&gt;=</option><option>=</option><option>&lt;</option><option>&lt;=</option><option>&lt;&gt;</option></select> <input name="number[]" type="text" size="2" maxlength="3"  class="step3" value="'.$row['value'].'"/> .</div>';
+	echo '<script type="text/javascript">document.getElementById("trigger'.$i.'").value='.$row['field_id'].';document.getElementById("operator'.$i.'").value="'.$row['operater'].'";</script>';
+	$i++;
+}
+?>
 
-</div>
+
+</div><br/>
 <div id="addcond">Add more trigger</div><br/>
 <input type="submit" value="Submit" name="submit"/>
 </form>
@@ -95,5 +128,17 @@ $(document).ready(function(){
 </body>
 </html>
 <?php
+}
+} else {
+	echo "<h2>Your triggers :</h2>";
+	$sql="select * from f_condition,f_fields where f_condition.field_id=f_fields.field_id and user_id=$myid";
+	$ret=mysql_query($sql) or die(mysql_error());
+	while ($row = mysql_fetch_array($ret)) {
+		echo "<div class='oldtrigger'><span style='display:none;'>".$row['trigger_id']."</span> <span class='teams'>".($row['isaway']?"Away":"Home")."</span> <span class='conditions'>".$row['field_name']."</span> <span class='operators'>".$row['operater']."</span> <span class='values'>".$row['value']."</span></div>";
+		
+	}
+	echo "<div><a href='trigger.php?edit'>Modify triggers</a></div>";
+	echo "<div><a href='index.php'>Go to home page</a></div>";
+	
 }
 ?>
