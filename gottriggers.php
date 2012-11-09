359dbd6f564e60700f3722c16fd8c1a4a6c58743
HuuNam0 <HuuNam0@gmail.com>
2012-11-09 16:23:30 +0000

    only show triggers for users who have twitter.

--- gottriggers.php
+++ gottriggers.php
@@ -9,18 +9,18 @@
 		$ret=mysql_query($sql2);
 		if ($usr = mysql_fetch_array($ret)) {
 			$user_twitter=$usr['user_twitter'];
-		}
-		echo "user#".$user_id."#".$user_twitter."\n";
-		$sql2="select f_matches.match_id,hometeam.team_name as homename,awayteam.team_name as awayname from f_matches ";
-		$sql2.="left join f_teams as hometeam on f_matches.hteam=hometeam.team_id ";
-		$sql2.="left join f_teams as awayteam on f_matches.ateam=awayteam.team_id ";
-		$sql2.="where (status between 1 and 6)and".$row['triggersm'];
-		//echo $sql2."<br/>";
-		$ret=mysql_query($sql2);
-		while ($data=mysql_fetch_array($ret)) {
-			echo "match#".$data['match_id']."#".$data['homename']." - ".$data['awayname']."\n";
-		}
 		
+			echo "user#".$user_id."#".$user_twitter."\n";
+			$sql2="select f_matches.match_id,hometeam.team_name as homename,awayteam.team_name as awayname from f_matches ";
+			$sql2.="left join f_teams as hometeam on f_matches.hteam=hometeam.team_id ";
+			$sql2.="left join f_teams as awayteam on f_matches.ateam=awayteam.team_id ";
+			$sql2.="where (status between 1 and 6)and".$row['triggersm'];
+			//echo $sql2."<br/>";
+			$ret=mysql_query($sql2);
+			while ($data=mysql_fetch_array($ret)) {
+				echo "match#".$data['match_id']."#".$data['homename']." - ".$data['awayname']."\n";
+			}
+		}
 		
 	}
 ?>
