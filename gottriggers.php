71ef1749610183b36cc8ee22b164738280d942c1
HuuNam0 <HuuNam0@gmail.com>
2013-03-18 15:54:18 +0000

    Modify gottriggers.php

--- gottriggers.php
+++ gottriggers.php
@@ -15,16 +15,24 @@
 		$ret=mysql_query($sql2);
 		if ($usr = mysql_fetch_array($ret)) {
 			$user_twitter=$usr['user_twitter'];
-			$sql2="select f_matches.match_id,hometeam.team_name as homename,awayteam.team_name as awayname from f_matches ";
+			$sql2="select f_matches.match_id,f_matches.hteam,f_matches.ateam,hometeam.team_name as homename,awayteam.team_name as awayname from f_matches ";
 			$sql2.="left join f_teams as hometeam on f_matches.hteam=hometeam.team_id ";
 			$sql2.="left join f_teams as awayteam on f_matches.ateam=awayteam.team_id ";
 			$sql2.="where (status between 1 and 6)and (viewdate='".$cdate."')and".(strlen($row['triggers'])>5?$row['triggers']:"(1=0)")."and(match_id not in (select match_id from f_sent where user_id=$user_id))";
-			echo "sql#".$sql2."#\n";
+			//echo "sql#".$sql2."#\n";
 			$ret=mysql_query($sql2);
 			if (mysql_num_rows($ret)) {
 				echo "user#".$user_id."#".$user_twitter."#\n";
 				while ($data=mysql_fetch_array($ret)) {
-					echo "match#".$data['match_id']."#".$data['homename']." - ".$data['awayname']."#\n";
+					$sql = "select team_id, team_pos from f_teams where team_id in (".$data['hteam'].",".$data['ateam'].")";
+					$ret2 = mysql_query($sql);
+					$homeinfo=$data['homename'];
+					$awayinfo=$data['awayname'];
+					while ($team = mysql_fetch_array($ret2)){
+						if ($team['team_id']==$data['hteam']) $homeinfo .= "(".$team['team_pos'].")";
+						if ($team['team_id']==$data['ateam']) $awayinfo .= "(".$team['team_pos'].")";
+					}
+					echo "match#".$data['match_id']."#".$homeinfo." v ".$awayinfo."#\n";
 				}
 			}
 		}
